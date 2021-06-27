da7a8b93ca24ba47c10e566f0444297b4bf2c0aa
Mário Sousa <mariojsxj@gmail.com>
2021-06-27 20:37:11 -0300

    Criação do projeto

---
+++ src/main/java/com/mario/umlProject/UmlProject1Application.java
@@ -0,0 +1,13 @@
+package com.mario.umlProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmlProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmlProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mario/umlProject/UmlProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mario.umlProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmlProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
